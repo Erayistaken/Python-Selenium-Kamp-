de6221dd9305d8978fa8776f37317534a4d91aec
Erayistaken <sahineray6060@gmail.com>
2023-03-09 21:28:12 +0300

    Python & Selenium Kampı 1.Hafta Ödevi

---
+++ Python & Selenium Kampı/Hafta 1/firstweekodev1.py
@@ -0,0 +1,38 @@
+# string:  Metinsel verileri saklamak istediğimiz zaman string veri tipini kullanırız. Çift tırnak kullanarak tanımlamamızı gerçekleştiririz.
+string = "Merhaba Python!"
+stringCumle = "Kodlamaio Selam!"
+
+# integer , int : Tam sayı ifadeler
+sayi1 = 15
+sayi2 = 10
+
+# Float , double , decimal : Ondalıklı sayı tutar.
+sayi3 = 18.90
+
+# Bool , booleam : True veya false 
+print(8 > 4)        # True
+print(8 == 4)       # False
+print(8 < 4)        # False
+
+# list : sıralı ve değiştirilebilir veri tipidir
+list1 =  ["istanbul , ankara"]
+
+# tuple : list veri tipi gibi ama değiştirilemez
+tuple("elma , armut")
+
+#-------------------------------------------------------------------------------------------
+
+#Kodlama.io sitesinden veri tipi örnekleri
+# string : Kurslarım,Yazilim Geliştirici Yetiştirme Kampi,Kariyer ve Bitir ve devam et string veri tipine örnek
+# int :  telefon numarası ,kurs tamamlama yüzdesi
+
+#-------------------------------------------------------------------------------------------
+#Kodlama.io sitesinde şart blokları kullanıldığını düşündüğünüz kısımları örneklendiriniz ve Python dilinde bu örnekleri koda dökünüz.
+
+bitirVeDevamEt = True
+
+if bitirVeDevamEt == "true":
+    print("Tebrikler ödevi tamamladınız diğer sayafaya geçiniz")
+else:
+    print("Ödevi tamamlamadınız")
+
